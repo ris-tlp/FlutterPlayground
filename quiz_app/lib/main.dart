58cdbe64927f1b9874ce8df837e4d757a0164b67
Omar Pervez Khan <omarkhantlp@gmail.com>
2021-08-17 14:15:12 +0300

    mvp

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,7 +1,7 @@
 import "package:flutter/material.dart";
 
-import "question.dart";
-import "answer.dart";
+import "quiz.dart";
+import "result.dart";
 
 void main() => runApp(MyApp());
 
@@ -14,27 +14,51 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  final questions = const [
+  final _questions = const [
     {
       "questionText": "What's your favorite color?",
-      "answers": ["Black", "Red", "Green", "White"]
+      "answers": [
+        {"text": "Black", "score": 10},
+        {"text": "Red", "score": 5},
+        {"text": "Green", "score": 3},
+        {"text": "White", "score": 5}
+      ]
     },
     {
       "questionText": "What's your favorite animal?",
-      "answers": ["Rabbit", "Snake", "Elephant", "Lion"]
+      "answers": [
+        {"text": "Rabbit", "score": 10},
+        {"text": "Snake", "score": 5},
+        {"text": "Elephant", "score": 3},
+        {"text": "Lion", "score": 1},
+      ]
     },
     {
       "questionText": "What's your favorite food?",
-      "answers": ["Biryani", "Biryani", "Biryani", "Biryani"]
+      "answers": [
+        {"text": "Biryani", "score": 1},
+        {"text": "Biryani", "score": 1},
+        {"text": "Biryani", "score": 1},
+        {"text": "Biryani", "score": 1},
+      ]
     }
   ];
 
-  void _answerQuestion() {
-    if (_questionIndex < questions.length)
-      setState(() {
-        _questionIndex = _questionIndex + 1;
-      });
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
   }
 
   @override
@@ -44,17 +68,13 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text("Quiz!"),
         ),
-        body: Column(
-          children: <Widget>[
-            Question(
-              questions[_questionIndex]["questionText"] as String,
-            ),
-            ...(questions[_questionIndex]["answers"] as List<String>)
-                .map((answer) {
-              return Answer(_answerQuestion, answer);
-            }).toList()
-          ],
-        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
